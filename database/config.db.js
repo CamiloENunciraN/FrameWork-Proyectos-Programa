64874afb5a3b0849d9c830802e5fc0b059932023
CamiloENunciraN <camiloeduardonn@ufps.edu.co>
2023-05-27 10:01:33 -0500

    error db

--- database/config.db.js
+++ database/config.db.js
@@ -1,4 +1,5 @@
 //dotenv nos permite leer las variables de entorno de nuestro .env
+/*
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -17,4 +18,26 @@ try {
     console.log("Error al conectar con la base de datos");
 }
 
+module.exports = {connection};
+*/
+const dotenv = require("dotenv");
+dotenv.config();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+        host: process.env.DBHOST,
+        port:process.env.DBPORT,
+        user: process.env.DBUSER,
+        password: process.env.DBPASS,
+        database: process.env.DBNAME
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error('Error al conectar a la base de datos: ' + error.stack);
+    return;
+  }
+
+  console.log('Conexión exitosa a la base de datos.');
+});
 module.exports = {connection};

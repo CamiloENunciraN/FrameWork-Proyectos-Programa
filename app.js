7247b2aecb949318ec19dbadf5ce6f419f4d6b5c
CamiloENunciraN <camiloeduardonn@ufps.edu.co>
2023-05-26 20:34:50 -0500

    cambio CORs

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const cors = require('cors');
+
 //nos ayuda a analizar el cuerpo de la solicitud POST
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors({origin: '*'}));
-app.use(cors({methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']}));
+
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+   // res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 // cargar el css del index.html as static
 app.use(express.static(__dirname));
--- routes/noticias.js
+++ routes/noticias.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
 const dotenv = require("dotenv");
 dotenv.config();
-app.use(cors({origin: '*'}));
-app.use(cors({methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']}));
 //conexión con la base de datos
 const {connection} = require("../database/config.db");
 
